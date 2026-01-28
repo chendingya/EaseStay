a389b6c676b2d0d2365f937cdda0b5e99b063f7d
shingeki <1328144075@qq.com>
2026-01-28 21:41:40 +0800

    修复启动问题 npm包问题

--- start.ps1
+++ start.ps1
@@ -1,9 +1,12 @@
 Set-Location $PSScriptRoot
+$env:PATH += ";C:\Windows\System32"
+
 if (-not (Test-Path "node_modules")) { npm install }
 if (-not (Test-Path "mobile\\node_modules")) { npm install --prefix mobile }
 if (-not (Test-Path "admin\\node_modules")) { npm install --prefix admin }
 if (-not (Test-Path "server\\node_modules")) { npm install --prefix server }
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "npm run dev:all"
+
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "$env:PATH += ';C:\Windows\System32'; npm run dev:all"
 Start-Sleep -Seconds 6
 Start-Process "http://localhost:10086/"
 Start-Process "http://localhost:4101/"
